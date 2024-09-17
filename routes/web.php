1cfc02cf33639eb8376267e7a8542631806728bd
kenta-afk <kentarou9.60@icloud.com>
2024-09-17 20:15:57 +0900

    コメント機能の追加

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\TweetController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TweetLikeController;
@@ -21,6 +22,8 @@ Route::middleware('auth')->group(function () {
     Route::resource('tweets', TweetController::class);
     Route::post('/tweets/{tweet}/like', [TweetLikeController::class, 'store'])->name('tweets.like');
     Route::delete('/tweets/{tweet}/like', [TweetLikeController::class, 'destroy'])->name('tweets.dislike');
+
+    Route::resource('tweets.comments', CommentController::class);
 });
 
 require __DIR__.'/auth.php';
